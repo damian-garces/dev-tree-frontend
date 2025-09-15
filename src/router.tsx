27a7dedf4d5016eb84cc7cc7ae7d6fd245053a7c
Damian Arturo Garces Paez <guners86@hotmail.com>
2025-09-15 16:38:59 +0400

    feat: add AppLayout, LinkTree, and Profile components with routing setup

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './views/Login';
 import Register from './views/Register';
 import AuthLayout from './layouts/AuthLayouth';
+import AppLayout from './layouts/AppLayout';
+import LinkTree from './views/LinkTree';
+import Profile from './views/Profile';
 
 export default function Router() {
   return (
@@ -10,6 +13,11 @@ export default function Router() {
         <Route element={<AuthLayout />}>
           <Route path="auth/login" element={<Login />} />
           <Route path="auth/register" element={<Register />} />
+        </Route>
+
+        <Route path="/admin" element={<AppLayout />}>
+          <Route index={true} element={<LinkTree />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
